e5a854bcfe6da55833dbde0d746c98be0dfa37d4
mengjinglei <mengjinglei@qiniu.com>
2015-11-06 15:06:20 +0800

    add config

--- influxdb.go
+++ influxdb.go
@@ -3,15 +3,22 @@ package main
 import (
 	"fmt"
 	client "lamo/tools/influxClient"
+	"sync"
 	"time"
 
 	"github.com/qiniu/log.v1"
 )
 
 type InfluxdbTester struct {
+	WriteN           uint64
+	FailN            int64
+	Psize            uint64
 	Conf             *Config
 	StopCh, ReportCh chan struct{}
+	CountCh          chan struct{}
+	FailCh           chan struct{}
 	Client           *client.InfluxClient
+	Wg               sync.WaitGroup
 }
 
 func NewInfluxdbTester(c *Config) *InfluxdbTester {
@@ -24,38 +31,105 @@ func NewInfluxdbTester(c *Config) *InfluxdbTester {
 	}
 
 	cli.Client = lbcli
+	var wg sync.WaitGroup
 	return &InfluxdbTester{
+		WriteN:   0,
+		Psize:    0,
 		Conf:     c,
 		StopCh:   make(chan struct{}),
 		ReportCh: make(chan struct{}),
+		CountCh:  make(chan struct{}),
+		FailCh:   make(chan struct{}),
 		Client:   cli,
+		Wg:       wg,
 	}
 }
 
 func (t *InfluxdbTester) Test() {
+	//ensure database
+	_, err := t.Client.Query("drop database testDB")
+	if err != nil {
+		log.Error(err)
+	}
+	_, err = t.Client.Query("create database testDB")
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
+	_, err = t.Client.Query("alter retention policy default on testDB duration 100d replication 1 ")
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
+	go func() {
+		for {
+			select {
+			case <-t.CountCh:
+				t.WriteN++
+			case <-t.StopCh:
+				break
+			case <-t.FailCh:
+				t.FailN++
+			}
+		}
+	}()
 
 	write := func(points []byte) {
 		for {
+
 			select {
 			case <-t.StopCh:
-				t.Report()
+				t.Wg.Done()
 				return
-			case <-t.ReportCh:
-				t.Report()
+			default:
 			}
 
-			t.Client.WritePoints(points)
+			err := t.Client.WritePoints(points)
+			if err != nil {
+				log.Error(err)
+				t.FailCh <- struct{}{}
+				continue
+			}
+			t.CountCh <- struct{}{}
 			time.Sleep(time.Duration(t.Conf.BatchInterval) * time.Millisecond)
 		}
 	}
+
 	for i := 0; i < int(t.Conf.Concurrency); i++ {
+		t.Wg.Add(1)
 		points := PointsGenerator(int(t.Conf.BatchSize))
+		t.Psize = uint64(len(points))
 		go write(points)
+	}
+
+	fmt.Printf("Point size:%d bytes\nPoints per Batch:%d\nBatch Size:%d bytes\n",
+		t.Psize/uint64(t.Conf.BatchSize), t.Conf.BatchSize, t.Psize)
+
+	select {
+	case <-time.After(time.Duration(t.Conf.TestTime) * time.Minute):
+		for i := 0; i <= int(t.Conf.Concurrency); i++ {
+			t.StopCh <- struct{}{}
+		}
+		//t.Wg.Wait()
+		t.Report()
 	}
 
 }
 
 func (t *InfluxdbTester) Report() {
-	fmt.Printf("Point size:%d bytes\nPoints per Batch:%d\nBatch Size:%d bytes\nWrite per second:%d\nPoints per Second:%d\nBytes per Second:%d\n",
-		1, 2, 2, 2, 2, 2)
+	t.Psize = t.Psize
+	wps := int64(t.WriteN) / 60 / t.Conf.TestTime
+	fmt.Printf("Write per second:%d\nPoints per Second:%d\nBytes per Second:%d\n",
+		wps, wps*int64(t.Conf.BatchSize), wps*int64(t.Psize))
+	fmt.Printf("write fail count:%d\n", t.FailN)
+	resp, err := t.Client.Query("select count(value) from cpu")
+	if err != nil {
+		log.Error(err)
+		return
+	} else {
+		fmt.Println(resp["results"])
+	}
+
 }
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"flag"
 	_ "fmt"
 	"net/http"
 	"time"
@@ -61,96 +60,6 @@ func main() {
 	}
 
 	tester.Test()
-
-	f := flag.String("f", "all", "specify which method to run:\n\t<all>: <create> + <write>\n\t<create>:create repoid,series and rp\n\t<write>: write data point\n\t<test>: =<all> operate direct on influxdb:8086")
-	URL := flag.String("url", "http://127.0.0.1:8899", "url when create metadata and write data point")
-	repo := flag.String("repo", "", "repoid")
-	repoN := flag.Int("repon", 1, "the number of repo to write data point to")
-	pointN := flag.Int("pointn", 1, "the number of points with one write operation")
-	interval := flag.Int64("n", 500, "ms")
-	minute := flag.Int64("minute", 16, "minute to run the test")
-	debug := flag.Bool("d", false, "debug, default false")
-	method := flag.String("method", "text", "write data points in application/json format or text/plain")
-	Cq := flag.Bool("cq", true, "specify whether create cq during writing data points, default false")
-	threadn := flag.Int("threadn", 1, "specify how many threads write data cocurrently")
-	Series := flag.String("series", "", "specify which series to write data in")
-
-	flag.Parse()
-
-	done := make(chan bool, 1)
-
-	if *f == "" {
-		log.Fatal("You have to specify the method to run!")
-		return
-	}
-
-	log.Printf("cmd:%s,url:%s,repo:%s,repoN:%d,interval:%d ms, method:%s", *f, *URL, *repo, *repoN, *interval, *method)
-
-	tr := &http.Transport{
-		DisableCompression: true,
-		DisableKeepAlives:  true,
-	}
-	client := &http.Client{Transport: tr}
-
-	if *f == "curl" {
-		client, err := newLbClient([]string{*URL})
-		if err != nil {
-			log.Debug(err)
-			return
-		}
-		Curl(client, *interval)
-		return
-	} else if *f == "test" {
-
-		WriteInfluxdb(*method, *interval)
-		return
-
-	} else if *f == "create" {
-
-		for i := 0; i < *repoN; i++ {
-			Create(*URL, *Cq)
-		}
-		return
-
-	} else if *f == "write" {
-		//client := &http.Client{}
-		if *Series == "" {
-			log.Fatal("series must specified")
-		}
-
-		job := InfluxJob{repoid: *repo, series: *Series, threadn: *threadn, interval: *interval, debug: *debug, method: *method, client: client, repoN: *repoN, pointN: *pointN, url: "http://" + *URL}
-
-		log.Info("start write repo:", *repo)
-		if *repo == "" {
-			log.Error("repo cannot be null!")
-		} else {
-			Write(job, *URL, "", *interval)
-		}
-		time.Sleep(time.Duration(*minute) * time.Minute)
-
-		return
-
-	} else if *f == "all" {
-
-		for i := 0; i < *repoN; i++ {
-			log.Info("start create")
-			repoid := Create(*URL, *Cq)
-			go func() {
-				log.Info("create repo", repoid, "complete")
-				log.Info("start to write", repoid, "to", *URL, "interval", *interval, "ms")
-				//client := &http.Client{}
-
-				job := InfluxJob{repoid: repoid, cq: *Cq, threadn: *threadn, interval: *interval, method: *method, debug: *debug, client: client, repoN: *repoN, pointN: *pointN, url: "http://" + *URL}
-
-				Write(job, *URL, "", *interval)
-			}()
-			time.Sleep(time.Duration(2) * time.Second)
-
-		}
-
-		//time.Sleep(time.Duration(*minute) * time.Minute)
-		<-done
-	}
 
 }
 
